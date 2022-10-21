7fdf8f232b65b2eff72417407eb05c9b7911c57e
Ingrid Liu <todearannie@gmail.com>
2022-10-20 20:30:10 -0700

    create route and controller

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
+// Postman: https://app.getpostman.com/join-team?invite_code=ec1609658824064deb784ec6d1b8f935&target_code=135223c5ce89b72bf6a15576f6cb73c7
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const port = 5000;
+const port = 5001;
 
-// Use middle-ware
+// use middle-ware
 app.use(cors());
 app.use(express.json());
 
+// use rountes
+app.use(require('./routes/route'));
+
 app.listen(port, ()=> {
-    console.log(`Server is running on port: ${port}...`)
+    console.log(`Server is running on port: ${port}`)
 })
