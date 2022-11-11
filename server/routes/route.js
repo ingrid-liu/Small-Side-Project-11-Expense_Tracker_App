1aa91ce3c1299cfa3fd98e5e484676320b473167
Ingrid Liu <todearannie@gmail.com>
2022-11-10 18:52:20 -0800

    controller

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,5 @@
 const routes = require('express').Router();
-const controller = require('../controller/controller');
+const controller = require('../controller/controller');     // require a file
 
 routes.route('/api/categories')
     .get(controller.create_Categories);
