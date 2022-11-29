6abef2c1d77e70cabb3ea82b03e35c4823513695
Ingrid Liu <todearannie@gmail.com>
2022-11-28 16:43:55 -0800

    CRUD routes & controllers setup

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,8 +1,77 @@
-// get categories
-function create_Categories(req, res){
-    res.json("Get Request from Categories")
- }
- 
- module.exports = {
-    create_Categories
- }
+const model = require("../models/model");
+
+// POST: http://localhost:8080/api/categories
+async function create_Categories(req, res) {
+  const Create = new model.Categories({
+    type: "Savings",
+    color: "#1F385C", // dark
+  });
+
+  // save the above info in MongoDB
+
+  await Create.save(function (err) {
+    if (!err) return res.json(Create);
+    return res
+      .status(400)
+      .json({ message: `Error while createing categories ${err}` });
+  });
+}
+
+// GET: http://localhost:8080/api/categories
+async function get_Categories(req, res) {
+  let data = await model.Categories.find({}); // inside find function, pass {} object -> return all the objects from the Categories collection
+
+  // filter the collection
+  let filter = await data.map((item) =>
+    Object.assign({}, { type: item.type, color: item.color })
+  );
+
+  return res.json(filter);
+}
+
+// POST: http://localhost:8080/api/transaction
+async function create_Transaction(req, res) {
+  if (!req.body) return res.status(400).json("Post HTTP Data not rovided");
+  let { name, type, amount } = req.body;
+
+  const create = await new model.Transaction({
+    name,
+    type,
+    date: new Date(),
+    amount,
+  });
+
+  create.save(function (err) {
+    if (!err) return res.json(create);
+    return res
+      .status(400)
+      .json({ message: `Error while creating transaction ${err}` });
+  });
+}
+
+// GET: http://localhost:8080/api/transaction
+async function get_Transaction(req, res) {
+  let data = await model.Transaction.find({});
+  return res.json(data);
+}
+
+// DELETE: http://localhost:8080/api/transaction
+async function delete_Transaction(req, res) {
+  if (!req.body) res.status(400).json({ message: "Request body not Found" });
+
+  await model.Transaction.deleteOne(req.body, function (err) {
+    if (!err) res.json("Record Deleted...");
+  })
+    .clone()
+    .catch(function (err) {
+      res.json("Error while deleting Trasaction Record");
+    });
+}
+
+module.exports = {
+  create_Categories,
+  get_Categories,
+  create_Transaction,
+  get_Transaction,
+  delete_Transaction,
+};
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,9 +1,15 @@
-const routes = require('express').Router();                 // require express framework
-const controller = require('../controller/controller');     // require a file
+const routes = require("express").Router(); // require express framework
+const controller = require("../controller/controller"); // require a file
 
-routes.route('/api/categories')
-    .get(controller.create_Categories);
+routes
+  .route("/api/categories")
+  .post(controller.create_Categories)
+  .get(controller.get_Categories);
 
+routes
+  .route("/api/transaction")
+  .post(controller.create_Transaction)
+  .get(controller.get_Transaction)
+  .delete(controller.delete_Transaction);
 
-
-module.exports=routes;
+module.exports = routes;
